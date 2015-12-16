5dbcaeb187d24c0e212a29321d071cf5b25638a4
GARETH COUCH <gcouch8@gmail.com>
2015-12-15 21:24:28 -0800

    css html and script updated

--- script.js
+++ script.js
@@ -10,14 +10,16 @@ var array = [];
 $(document).ready(function() {
   $("input:radio[name=neighborhood]").click(function(){
     $('#showResults').empty();
-    $('#showResults').append('<table></table>');
-    $('#showResults').append('<thead><tr><th>' + 'Name/Website' + '</th><th>' + 'Cost' + '</th> </tr></thead>');
+    $('#showResults').append('<section class="display"></section>');
+    $('#showResults').append('<h1 class="title">' + 'Name/Website' + '</h1>');
 
     var $selectedPlace = $(this).val();
 
 
       if($selectedPlace === 'fremont'){
       $('.hood').remove();
+      $('.hoodTitle').remove();
+      $('<h3 class="costTitle">Choose a Cost Range</h3>').appendTo('.cost');
       $('<input type="radio" name="costCheckbox" value="$">$<br>').appendTo('.cost');
       $('<input type="radio" name="costCheckbox" value="$$">$$<br>').appendTo('.cost');
       $('<input type="radio" name="costCheckbox" value="$$$">$$$<br>').appendTo('.cost');
@@ -29,23 +31,22 @@ $(document).ready(function() {
           //console.log($selectedCost);
           if($selectedCost === burp.cost){
             console.log(burp.cost);
-            $('#whoaResults').append('<table><tr>' + '<a href=' + burp.url + '>'
-              + burp.name + '</a></tr></table>');
-            $('#whoaResults').append('<td>' + '<img src=images/images-fremont/' + burp.id + '.jpg height=200 width=200>' + '</td>');
+            $('#whoaResults').append('<p>' + '<img src=images/images-fremont/' + burp.id + '.jpg height=200 width=200>' + '<a href=' + burp.url + '>'
+              + burp.name + '</a>' + '</p>');
+            //$('#whoaResults').append('<p>' + '<a href=' + burp.url + '>'
+              //+ burp.name + '</a></p>');
           }
         });
       });
       for(var bb = 0; bb < db.fremont.length; bb+= 1){
       array.push(db.fremont[bb]);
       console.log(db.fremont[bb].name);
-      $('#showResults').append('<tr>');
-      $('#showResults').append('<td>' + '<a href=' + db.fremont[bb].url + '>' + db.fremont[bb].name + '</a>' + '</td>');
-      $('#showResults').append('<td>' + db.fremont[bb].cost + '</td>');
-      $('#showResults').append('</tr>');
-      $('#showResults').append('<td>' + '<img src=images/images-fremont/' + db.fremont[bb].id + '.jpg height=200 width=200>' + '</td>');
+      $('#showResults').append('<p>' + '<img src=images/images-fremont/' + db.fremont[bb].id + '.jpg height=200 width=200>' + '<a href=' + db.fremont[bb].url + '>' + db.fremont[bb].name + '</a>' + '</p>');
       }
   } else if ($selectedPlace === 'capitolHill'){
       $('.hood').remove();
+      $('.hoodTitle').remove();
+      $('<h3 class="costTitle">Choose a Cost Range</h3>').appendTo('.cost');
       $('<input type="radio" name="costCheckbox" value="$">$<br>').appendTo('.cost');
       $('<input type="radio" name="costCheckbox" value="$$">$$<br>').appendTo('.cost');
       $('<input type="radio" name="costCheckbox" value="$$$">$$$<br>').appendTo('.cost');
@@ -56,24 +57,23 @@ $(document).ready(function() {
         array.forEach(function(burp){
           if($selectedCost === burp.cost){
             console.log(burp.cost);
-            $('#whoaResults').append('<table><tr>' + '<a href=' + burp.url + '>'
-              + burp.name + '</a></tr></table>');
-             $('#whoaResults').append('<td>' + '<img src=images/images-capitolHill/' + burp.id + '.jpg height=200 width=200>' + '</td>');
+             $('#whoaResults').append('<p>' + '<img src=images/images-capitolHill/' + burp.id + '.jpg height=200 width=200>' + '<a href=' + burp.url + '>'
+              + burp.name + '</a>' + '</p>');
+            //$('#whoaResults').append('<p>' + '<a href=' + burp.url + '>'
+              //+ burp.name + '</a></p>');
           }
         });
       });
       for(var bb = 0; bb < db.capitolHill.length; bb+= 1){
         array.push(db.capitolHill[bb]);
         console.log(db.capitolHill[bb].name);
-        $('#showResults').append('<tr>');
-        $('#showResults').append('<td>' + '<a href=' + db.capitolHill[bb].url + '>' + db.capitolHill[bb].name + '</a>' + '</td>');
-        $('#showResults').append('<td>' + db.capitolHill[bb].cost + '</td>');
-        $('#showResults').append('</tr>');
-         $('#showResults').append('<td>' + '<img src=images/images-capitolHill/' + db.capitolHill[bb].id + '.jpg height=200 width=200>' + '</td>');
+         $('#showResults').append('<p>' + '<img src=images/images-capitolHill/' + db.capitolHill[bb].id + '.jpg height=200 width=200>' + '<a href=' + db.capitolHill[bb].url + '>' + db.capitolHill[bb].name + '</a>' + '</p>');
       }
     } else if ($selectedPlace === 'ballard'){
 
       $('.hood').remove();
+      $('.hoodTitle').remove();
+      $('<h3 class="costTitle">Choose a Cost Range</h3>').appendTo('.cost');
       $('<input type="radio" name="costCheckbox" value="$">"$"<br>').appendTo('.cost');
       $('<input type="radio" name="costCheckbox" value="$$">"$$"<br>').appendTo('.cost');
       $('<input type="radio" name="costCheckbox" value="$$$">"$$$"<br>').appendTo('.cost');
@@ -84,25 +84,24 @@ $(document).ready(function() {
         array.forEach(function(burp){
           if($selectedCost === burp.cost){
             console.log(burp.cost);
-            $('#whoaResults').append('<table><tr>' + '<a href=' + burp.url + '>'
-              + burp.name + '</a></tr></table>');
-            $('#whoaResults').append('<td>' + '<img src=images/images-ballard/' + burp.id + '.jpg height=200 width=200>' + '</td>');
+            $('#whoaResults').append('<p>' + '<img src=images/images-ballard/' + burp.id + '.jpg height=200 width=200>' + '<a href=' + burp.url + '>'
+              + burp.name + '</a>' + '</p>');
+            //$('#whoaResults').append('<p>' + '<a href=' + burp.url + '>'
+              //+ burp.name + '</a></p>');
           }
         });
       });
         for(var bb = 0; bb < db.ballard.length; bb+= 1){
           array.push(db.ballard[bb]);
         console.log(db.ballard[bb].name);
-        $('#showResults').append('<tr>');
+
         //writing an anchor tag (activity name) then attaching link
-        $('#showResults').append('<td>' + '<a href=' + db.ballard[bb].url + ' target="_blank">' + db.ballard[bb].name + '</a>' + '</td>');
-        $('#showResults').append('<td>' + db.ballard[bb].cost + '</td>');
-        $('#showResults').append('</tr>');
-        $('#showResults').append('<td>' + '<img src=images/images-ballard/' + db.ballard[bb].id + '.jpg height=200 width=200>' + '</td>');
+        $('#showResults').append('<p>' + '<img src=images/images-ballard/' + db.ballard[bb].id + '.jpg height=200 width=200>' + '<a href=' + db.ballard[bb].url + ' target="_blank">' + db.ballard[bb].name + '</a>' +'</p>');
         }
     } else if ($selectedPlace === 'downtown'){
-
       $('.hood').remove();
+      $('.hoodTitle').remove();
+      $('<h3 class="costTitle">Choose a Cost Range</h3>').appendTo('.cost');
       $('<input type="radio" name="costCheckbox" value="$">"$"<br>').appendTo('.cost');
       $('<input type="radio" name="costCheckbox" value="$$">"$$"<br>').appendTo('.cost');
       $('<input type="radio" name="costCheckbox" value="$$$">"$$$"<br>').appendTo('.cost');
@@ -114,24 +113,23 @@ $(document).ready(function() {
           //console.log($selectedCost);
           if($selectedCost === burp.cost){
             console.log(burp.cost);
-            $('#whoaResults').append('<table><tr>' + '<a href=' + burp.url + '>'
-              + burp.name + '</a></tr></table>');
-            $('#whoaResults').append('<td>' + '<img src=images/images-downtown/' + burp.id + '.jpg height=200 width=200>' + '</td>');
+            $('#whoaResults').append('<p>' + '<img src=images/images-downtown/' + burp.id + '.jpg height=200 width=200>' + '<a href=' + burp.url + '>'
+              + burp.name + '</a>' + '</p>');
+            //$('#whoaResults').append('<p>' + '<a href=' + burp.url + '>'
+              //+ burp.name + '</a></p>');
           }
         });
       });
         for(var bb = 0; bb < db.downtown.length; bb+= 1){
           array.push(db.downtown[bb]);
         console.log(db.downtown[bb].name);
-        $('#showResults').append('<tr>');
-        $('#showResults').append('<td>' + '<a href=' + db.downtown[bb].url + '>' + db.downtown[bb].name + '</a>' + '</td>');
-        $('#showResults').append('<td>' + db.downtown[bb].cost + '</td>');
-        $('#showResults').append('</tr>');
-        $('#showResults').append('<td>' + '<img src=images/images-downtown/' + db.downtown[bb].id + '.jpg height=200 width=200>' + '</td>');
+        $('#showResults').append('<p>' + '<img src=images/images-downtown/' + db.downtown[bb].id + '.jpg height=200 width=200>' + '<a href=' + db.downtown[bb].url + '>' + db.downtown[bb].name + '</a>' + '</p>');
         }
     } else if ($selectedPlace === 'queenAnne'){
 
       $('.hood').remove();
+      $('.hoodTitle').remove();
+      $('<h3 class="costTitle">Choose a Cost Range</h3>').appendTo('.cost');
       $('<input type="radio" name="costCheckbox" value="$">"$"<br>').appendTo('.cost');
       $('<input type="radio" name="costCheckbox" value="$$">"$$"<br>').appendTo('.cost');
       $('<input type="radio" name="costCheckbox" value="$$$">"$$$"<br>').appendTo('.cost');
@@ -143,20 +141,17 @@ $(document).ready(function() {
           //console.log($selectedCost);
           if($selectedCost === burp.cost){
             console.log(burp.cost);
-            $('#whoaResults').append('<table><tr>' + '<a href=' + burp.url + '>'
-              + burp.name + '</a></tr></table>');
-            $('#whoaResults').append('<td>' + '<img src=images/images-queenAnne/' + burp.id + '.jpg height=200 width=200>' + '</td>');
+            $('#whoaResults').append('<p>' + '<img src=images/images-queenAnne/' + burp.id + '.jpg height=200 width=200>' + '<a href=' + burp.url + '>'
+              + burp.name + '</a>' + '</p>');
+            //$('#whoaResults').append('<p>' + '<a href=' + burp.url + '>'
+              //+ burp.name + '</a></p>');
           }
         });
       });
         for(var bb = 0; bb < db.queenAnne.length; bb+= 1){
           array.push(db.queenAnne[bb]);
         console.log(db.queenAnne[bb].name);
-        $('#showResults').append('<tr>');
-        $('#showResults').append('<td>' + '<a href=' + db.queenAnne[bb].url + '>' + db.queenAnne[bb].name + '</a>' + '</td>');
-        $('#showResults').append('<td>' + db.queenAnne[bb].cost + '</td>');
-        $('#showResults').append('</tr>');
-        $('#showResults').append('<td>' + '<img src=images/images-queenAnne/' + db.queenAnne[bb].id + '.jpg height=200 width=200>' + '</td>');
+        $('#showResults').append('<p>' + '<img src=images/images-queenAnne/' + db.queenAnne[bb].id + '.jpg height=200 width=200>' + '<a href=' + db.queenAnne[bb].url + '>' + db.queenAnne[bb].name + '</a>' + '</p>');
         }
     };
    });
