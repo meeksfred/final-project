baeca08d90f6a498a4c52f92c99be72da240927f
meeksfred <maxfred0025@gmail.com>
2015-12-17 20:55:32 -0800

    css near completion

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
-// function find() {
-// for (var x=0;x < db.ballard.length;x++) {
-//   if (db.ballard[x].cost < 2) {
-//     console.log(db.ballard[x].name);
-//   }
-// }
-// }
+
 var array = [];
 
 $(document).ready(function() {
   $("input:radio[name=neighborhood]").click(function(){
     $('#showResults').empty();
     $('#showResults').append('<section class="display"></section>');
-    $('#showResults').append('<h1 class="title">' + 'Name/Website' + '</h1>');
 
     var $selectedPlace = $(this).val();
 
@@ -20,21 +13,18 @@ $(document).ready(function() {
       $('.hood').remove();
       $('.hoodTitle').remove();
       $('<h3 class="costTitle">Choose a Cost Range</h3>').appendTo('.cost');
-      $('<input type="radio" name="costCheckbox" value="$">$<br>').appendTo('.cost');
-      $('<input type="radio" name="costCheckbox" value="$$">$$<br>').appendTo('.cost');
-      $('<input type="radio" name="costCheckbox" value="$$$">$$$<br>').appendTo('.cost');
+      $('<input id="$" type="radio" name="costCheckbox" value="$"><label for="$">$</label></br>').appendTo('.cost');
+      $('<input id="$$" type="radio" name="costCheckbox" value="$$"><label for="$$">$$</label></br>').appendTo('.cost');
+      $('<input id="$$$" type="radio" name="costCheckbox" value="$$$"><label for="$$$">$$$</label></br>').appendTo('.cost');
       $("input:radio[name='costCheckbox']").click(function(){
         var $selectedCost = $(this).val();
         $('#showResults').remove();
         $('#whoaResults').empty();
         array.forEach(function(burp){
-          //console.log($selectedCost);
           if($selectedCost === burp.cost){
             console.log(burp.cost);
             $('#whoaResults').append('<p>' + '<img src=images/images-fremont/' + burp.id + '.jpg height=200 width=200>' + '<a href=' + burp.url + '>'
               + burp.name + '</a>' + '</p>');
-            //$('#whoaResults').append('<p>' + '<a href=' + burp.url + '>'
-              //+ burp.name + '</a></p>');
           }
         });
       });
@@ -47,9 +37,9 @@ $(document).ready(function() {
       $('.hood').remove();
       $('.hoodTitle').remove();
       $('<h3 class="costTitle">Choose a Cost Range</h3>').appendTo('.cost');
-      $('<input type="radio" name="costCheckbox" value="$">$<br>').appendTo('.cost');
-      $('<input type="radio" name="costCheckbox" value="$$">$$<br>').appendTo('.cost');
-      $('<input type="radio" name="costCheckbox" value="$$$">$$$<br>').appendTo('.cost');
+      $('<input id="$" type="radio" name="costCheckbox" value="$"><label for="$">$</label></br>').appendTo('.cost');
+      $('<input id="$$" type="radio" name="costCheckbox" value="$$"><label for="$$">$$</label></br>').appendTo('.cost');
+      $('<input id="$$$" type="radio" name="costCheckbox" value="$$$"><label for="$$$">$$$</label></br>').appendTo('.cost');
       $("input:radio[name='costCheckbox']").click(function(){
         var $selectedCost = $(this).val();
         $('#showResults').remove();
@@ -59,8 +49,6 @@ $(document).ready(function() {
             console.log(burp.cost);
              $('#whoaResults').append('<p>' + '<img src=images/images-capitolHill/' + burp.id + '.jpg height=200 width=200>' + '<a href=' + burp.url + '>'
               + burp.name + '</a>' + '</p>');
-            //$('#whoaResults').append('<p>' + '<a href=' + burp.url + '>'
-              //+ burp.name + '</a></p>');
           }
         });
       });
@@ -74,9 +62,9 @@ $(document).ready(function() {
       $('.hood').remove();
       $('.hoodTitle').remove();
       $('<h3 class="costTitle">Choose a Cost Range</h3>').appendTo('.cost');
-      $('<input type="radio" name="costCheckbox" value="$">"$"<br>').appendTo('.cost');
-      $('<input type="radio" name="costCheckbox" value="$$">"$$"<br>').appendTo('.cost');
-      $('<input type="radio" name="costCheckbox" value="$$$">"$$$"<br>').appendTo('.cost');
+      $('<input id="$" type="radio" name="costCheckbox" value="$"><label for="$">$</label></br>').appendTo('.cost');
+      $('<input id="$$" type="radio" name="costCheckbox" value="$$"><label for="$$">$$</label></br>').appendTo('.cost');
+      $('<input id="$$$" type="radio" name="costCheckbox" value="$$$"><label for="$$$">$$$</label></br>').appendTo('.cost');
       $("input:radio[name='costCheckbox']").click(function(){
         var $selectedCost = $(this).val();
         $('#showResults').remove();
@@ -86,8 +74,6 @@ $(document).ready(function() {
             console.log(burp.cost);
             $('#whoaResults').append('<p>' + '<img src=images/images-ballard/' + burp.id + '.jpg height=200 width=200>' + '<a href=' + burp.url + '>'
               + burp.name + '</a>' + '</p>');
-            //$('#whoaResults').append('<p>' + '<a href=' + burp.url + '>'
-              //+ burp.name + '</a></p>');
           }
         });
       });
@@ -95,28 +81,24 @@ $(document).ready(function() {
           array.push(db.ballard[bb]);
         console.log(db.ballard[bb].name);
 
-        //writing an anchor tag (activity name) then attaching link
         $('#showResults').append('<p>' + '<img src=images/images-ballard/' + db.ballard[bb].id + '.jpg height=200 width=200>' + '<a href=' + db.ballard[bb].url + ' target="_blank">' + db.ballard[bb].name + '</a>' +'</p>');
         }
     } else if ($selectedPlace === 'downtown'){
       $('.hood').remove();
       $('.hoodTitle').remove();
       $('<h3 class="costTitle">Choose a Cost Range</h3>').appendTo('.cost');
-      $('<input type="radio" name="costCheckbox" value="$">"$"<br>').appendTo('.cost');
-      $('<input type="radio" name="costCheckbox" value="$$">"$$"<br>').appendTo('.cost');
-      $('<input type="radio" name="costCheckbox" value="$$$">"$$$"<br>').appendTo('.cost');
+      $('<input id="$" type="radio" name="costCheckbox" value="$"><label for="$">$</label></br>').appendTo('.cost');
+      $('<input id="$$" type="radio" name="costCheckbox" value="$$"><label for="$$">$$</label></br>').appendTo('.cost');
+      $('<input id="$$$" type="radio" name="costCheckbox" value="$$$"><label for="$$$">$$$</label></br>').appendTo('.cost');
       $("input:radio[name='costCheckbox']").click(function(){
         var $selectedCost = $(this).val();
         $('#showResults').remove();
         $('#whoaResults').empty();
         array.forEach(function(burp){
-          //console.log($selectedCost);
           if($selectedCost === burp.cost){
             console.log(burp.cost);
             $('#whoaResults').append('<p>' + '<img src=images/images-downtown/' + burp.id + '.jpg height=200 width=200>' + '<a href=' + burp.url + '>'
               + burp.name + '</a>' + '</p>');
-            //$('#whoaResults').append('<p>' + '<a href=' + burp.url + '>'
-              //+ burp.name + '</a></p>');
           }
         });
       });
@@ -130,21 +112,18 @@ $(document).ready(function() {
       $('.hood').remove();
       $('.hoodTitle').remove();
       $('<h3 class="costTitle">Choose a Cost Range</h3>').appendTo('.cost');
-      $('<input type="radio" name="costCheckbox" value="$">"$"<br>').appendTo('.cost');
-      $('<input type="radio" name="costCheckbox" value="$$">"$$"<br>').appendTo('.cost');
-      $('<input type="radio" name="costCheckbox" value="$$$">"$$$"<br>').appendTo('.cost');
+      $('<input id="$" type="radio" name="costCheckbox" value="$"><label for="$">$</label></br>').appendTo('.cost');
+      $('<input id="$$" type="radio" name="costCheckbox" value="$$"><label for="$$">$$</label></br>').appendTo('.cost');
+      $('<input id="$$$" type="radio" name="costCheckbox" value="$$$"><label for="$$$">$$$</label></br>').appendTo('.cost');
       $("input:radio[name='costCheckbox']").click(function(){
         var $selectedCost = $(this).val();
         $('#showResults').remove();
         $('#whoaResults').empty();
         array.forEach(function(burp){
-          //console.log($selectedCost);
           if($selectedCost === burp.cost){
             console.log(burp.cost);
             $('#whoaResults').append('<p>' + '<img src=images/images-queenAnne/' + burp.id + '.jpg height=200 width=200>' + '<a href=' + burp.url + '>'
               + burp.name + '</a>' + '</p>');
-            //$('#whoaResults').append('<p>' + '<a href=' + burp.url + '>'
-              //+ burp.name + '</a></p>');
           }
         });
       });
@@ -156,39 +135,3 @@ $(document).ready(function() {
     };
    });
    });
-
-
-
-
-
-
-
-    //this code will only be run after an initial neighborhood(s) has been
-    //selected. Once the user hits submit to find activities in a neighborhood,
-        //the neighborhood checkboxes will disappear.
-        //I'll then loop through the matches array to determine how many
-         // $('.neighborhood').remove();
-         // for (i = 0; i < 3; i++) {
-         // $('<input type="checkbox" name="costCheckbox" value=""/>').appendTo('.cost');
-         // }
-         // $('<input type="button" name="costButton" value=""/>').appendTo('.cost');
-
-// $('form').click(function(){
-//     $(this).css("background-color", "yellow");
-// })
-
-// $('input[type=checkbox]').change(
-//     function(){
-//         if (this.checked) {
-//             add
-//             alert('checked');
-//         }
-//     });
-
-
-// $('input[type=checkbox]').change(
-//     function(){
-//         if (this.checked) {
-//             alert('checked');
-//         }
-//     });
